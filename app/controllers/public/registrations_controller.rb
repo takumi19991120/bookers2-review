c15485b8568534edd6c88e7eb9c105c2da7dba12
takumi19991120 <135395048+takumi19991120@users.noreply.github.com>
2023-09-12 14:29:45 +0000

    [finish]homes

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -3,7 +3,26 @@
 class Public::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  def new
+    super
+  end
 
+  def create
+    super
+  end
+
+  def after_sign_up_path_for(resource)
+    user_path(current_user.id)
+  end
+  
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :name])
+  end
   # GET /resource/sign_up
   # def new
   #   super
