6b8e81e86fd0046d4ddcdfd0422bff716a9b4816
takumi19991120 <135395048+takumi19991120@users.noreply.github.com>
2023-09-14 07:39:10 +0000

    [finish]homes

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -7,4 +7,11 @@ class Public::UsersController < ApplicationController
 
   def edit
   end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+  
 end
